34a3e035d5b07b687f50639088b835b907cfa90b
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-20 22:15:49 +0530

    [akhila]Refactor:Searching value in tree

--- BinarySearchTree/BinarySearchTree/BinaryTree.cs
+++ BinarySearchTree/BinarySearchTree/BinaryTree.cs
@@ -8,16 +8,15 @@ namespace BinarySearchTree
     /// creating a Binary Search Tree 
     /// </summary>
     /// <typeparam name="T"></typeparam>
-
     class BinaryTree<T> where T : IComparable
     {
-        T INode;       
-        public BinaryTree<T> leftnode{ get; set; }
-        public BinaryTree<T> rightnode{ get; set; }      
+        T INode;
+        public BinaryTree<T> leftnode { get; set; }
+        public BinaryTree<T> rightnode { get; set; }
 
         public BinaryTree(T INode)
-        {            
-            this.INode = INode;           
+        {
+            this.INode = INode;
             this.leftnode = null;
             this.rightnode = null;
         }
@@ -33,8 +32,7 @@ namespace BinarySearchTree
                 if (this.leftnode == null)
                     this.leftnode = new BinaryTree<T>(data);
                 else
-                    this.leftnode.Add(data);
-            }
+                    this.leftnode.Add(data);            }
             else
             {
                 if (this.rightnode == null)
@@ -42,18 +40,41 @@ namespace BinarySearchTree
                 else
                     this.rightnode.Add(data);
             }
-            
+        }
+        /// <summary>
+        /// searching the value in binary tree
+        /// </summary>
+        /// <param name="arr">array</param>
+        /// <param name="l">left</param>
+        /// <param name="r">right</param>
+        /// <param name="x">index</param>
+        /// <returns></returns>
+        internal int BinarySearch(int[] arr, int l, int r, int x)
+        {
+            if (r >= l)
+            {
+                int mid = l + (r - l) / 2;
+                if (arr[mid] == x)
+                    return mid;
+                if (arr[mid] > x)
+                    return BinarySearch(arr, l, mid - 1, x);
+                return BinarySearch(arr, mid + 1, r, x);
+            }
+            return -1;
         }
         /// <summary>
         /// display the BST list
         /// </summary>
         public void Display()
         {
-            if (this.leftnode != null)            
+            if (this.leftnode != null)
                 this.leftnode.Display();
             Console.WriteLine(this.INode);
-             if (this.rightnode != null)            
-                this.rightnode.Display();           
+            if (this.rightnode != null)
+                this.rightnode.Display();
         }
     }
+
+
 }
+    
--- BinarySearchTree/BinarySearchTree/Program.cs
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -10,8 +10,20 @@ namespace BinarySearchTree
             BinaryTree<int> binaryTree = new BinaryTree<int>(56);
             binaryTree.Add(30);
             binaryTree.Add(70);
+            binaryTree.Add(12);
+            binaryTree.Add(66);            
             Console.WriteLine("Binary Search Tree is:-");
-            binaryTree.Display();           
+            binaryTree.Display();
+            int[] arr = { 12,30,56,66,70 };
+            int n = arr.Length;
+            int x = 70;
+            int result = binaryTree.BinarySearch(arr, 0, n - 1, x);
+            if (result == -1)
+                Console.WriteLine("Element not found");
+            else
+                Console.WriteLine("Element found at index "
+                                  + result);
         }
     }
+    
 }
